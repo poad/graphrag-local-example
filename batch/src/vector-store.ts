18a1205b72d31425bb91ab707768447d15bae0e7
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-25 17:49:02 +0900

    Bumps node modules

--- batch/src/vector-store.ts
+++ batch/src/vector-store.ts
@@ -1,10 +1,15 @@
-import 'neo4j-driver';
-
-import { Neo4jVectorStore } from '@langchain/community/vectorstores/neo4j_vector';
 import { Embeddings } from '@langchain/core/embeddings';
-
+import { Neo4jVectorStore } from '@langchain/community/vectorstores/neo4j_vector';
 import 'source-map-support/register.js';
 
+/**
+ * Create a new vector search index that uses the existing
+ * `moviePlots` index.
+ *
+ * @param {EmbeddingsInterface} embeddings  The embeddings model
+ * @returns {Promise<Neo4jVectorStore>}
+ */
+// tag::function[]
 
 async function createVectorStore({
   embeddings,
@@ -14,25 +19,38 @@ async function createVectorStore({
   indexName: string
 }) {
   try {
-    return new Neo4jVectorStore(
-      embeddings, {
-        url: process.env.NEO4J_URL ?? '',
-        username: process.env.NEO4J_USERNAME ?? 'neo4j',
-        password: process.env.NEO4J_PASSWORD ?? '',
-        indexName,
-        keywordIndexName: 'keyword',
-        searchType: 'vector' as const,
-        nodeLabel: 'Chunk',
-        textNodeProperty: 'text',
-        embeddingNodeProperty: 'embedding',
-        preDeleteCollection: true,
-        createIdIndex: true,
-      },
-    );
+    // tag::store[]
+    const vectorStore = await Neo4jVectorStore.fromExistingIndex(embeddings, {
+      url: process.env.NEO4J_URI as string,
+      username: process.env.NEO4J_USERNAME as string,
+      password: process.env.NEO4J_PASSWORD as string,
+      indexName,
+      textNodeProperty: 'text',
+      embeddingNodeProperty: 'embedding',
+      retrievalQuery: `
+      RETURN
+        node.plot AS text,
+        score,
+        {
+          _id: elementid(node),
+          title: node.title,
+          directors: [ (person)-[:DIRECTED]->(node) | person.name ],
+          actors: [ (person)-[r:ACTED_IN]->(node) | [person.name, r.role] ],
+          tmdbId: node.tmdbId,
+          source: 'https://www.themoviedb.org/movie/'+ node.tmdbId
+        } AS metadata
+    `,
+    });
+    // end::store[]
+
+    // tag::return[]
+    return vectorStore;
+    // end::return[]
   } catch (e) {
     console.error(e);
     throw e;
   }
 }
+// end::function[]
 
 export { createVectorStore };
